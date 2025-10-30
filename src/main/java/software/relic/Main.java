4d4a3d03fb57ee1214c1c972a3d878b0e8016cc1
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2025-10-30 18:30:15 +0000

    Initial commit: base code for Datalake refactor exercise (vendor lock-in reduction)

---
+++ src/main/java/software/relic/Main.java
@@ -0,0 +1,16 @@
+package software.relic;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		try (WebService webService = new WebService()) {
+
+		}
+
+	}
+
+
+
+}
